f3f1f011df0051ed02401c690f6371baca5a8cdc
Abdullah-Mubark <32361577+Abdullah-Mubark@users.noreply.github.com>
2020-03-19 03:07:21 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ if (!fs.existsSync('./output')){
     fs.mkdirSync('./output');
 }
 
-// Parse Stocks list 
-let stocksList = JSON.parse(stocksRawdata);
+// Parse stocks list & filter stocks list with only the active stocks
+let stocksList = JSON.parse(stocksRawdata).filter(stock => stock.isActive);
 
 let requestsList = setUpHttpRequests(stocksList);
 
@@ -35,8 +35,6 @@ function printAllStocksTextFile() {
   let textData = 'السهم | السعر\n'
   textData += '-----------------\n'
   stocksList.forEach((stock) => {
-	// Skip inactive stocks
-	if(stock.isActive == false) return;
     let line = stock.name + ' --> ' + getLatestStockPrice(stock.data);
     textData += line + '\n';
     textData += '-----------------\n';
@@ -48,8 +46,6 @@ function printAllStocksTextFile() {
 function printAllStocksJsonFile() {
   let jsonData = [];
   stocksList.forEach((stock) => {
-	// Skip inactive stocks
-	if(stock.isActive == false) return;
     const stockObject = {};
     stockObject[stock.name] = getLatestStockPrice(stock.data);
     jsonData.push(stockObject);
@@ -68,13 +64,19 @@ function printAllStocksExcelFile(){
 	  font: {
 		bold: true,
 		color: '#ff0000',
-		size: 18
+		size: 16
 	  },
 	   alignment: { 
 			 horizontal: ['center'],
 			 readingorder: ['righttoleft'], 
 			 vertical: ['center']
-	  }
+	  },
+	   border: {
+		   bottom: {
+            style: 'medium',
+            color: '#000000'
+        }
+	   }
 	});
 	const headersStyle_Calculated = workbook.createStyle({
 	  font: {
@@ -86,7 +88,13 @@ function printAllStocksExcelFile(){
 			 horizontal: ['center'],
 			 readingorder: ['righttoleft'], 
 			 vertical: ['center']
-	  }
+	  },
+	   border: {
+		   bottom: {
+            style: 'medium',
+            color: '#000000'
+        }
+	   }
 	});
 	
 	const rowsStyle = workbook.createStyle({
@@ -103,34 +111,36 @@ function printAllStocksExcelFile(){
 	});
 
 	// First row is static:
+	// Stock Market Type
+	worksheet.cell(1,1).string('القطاع').style(headersStyle_Static);
 	// Company Name
-	worksheet.cell(1,1).string('الشركة').style(headersStyle_Static);
+	worksheet.cell(1,2).string('الشركة').style(headersStyle_Static);
 	// Stock Price
-	worksheet.cell(1,2).string('سعر السهم').style(headersStyle_Calculated);
+	worksheet.cell(1,3).string('سعر السهم').style(headersStyle_Calculated);
 	// Dividend Average for the last three years
-	worksheet.cell(1,3).string('متوسط التوزيعات').style(headersStyle_Static);
+	worksheet.cell(1,4).string('متوسط التوزيعات').style(headersStyle_Static);
 	// Dividend Yield
-	worksheet.cell(1,4).string('مكرر التوزيعات').style(headersStyle_Calculated);
+	worksheet.cell(1,5).string('مكرر التوزيعات').style(headersStyle_Calculated);
 	// Average Cost
-	worksheet.cell(1,5).string('متوسط الشراء').style(headersStyle_Static);
+	worksheet.cell(1,6).string('متوسط الشراء').style(headersStyle_Static);
 	// Current stock cost to its average cost
-	worksheet.cell(1,6).string('نسبة الفرق عن متوسط الشراء').style(headersStyle_Calculated);
+	worksheet.cell(1,7).string('نسبة الفرق عن متوسط الشراء').style(headersStyle_Calculated);
 	
-	// First row is a filtrable headers
-	worksheet.row(1).filter(1, 2, 3, 4);
-	
+	// Set First row  hieght and is its cells as a filtrable headers
+	worksheet.row(1).setHeight(35);
+	worksheet.row(1).filter(1, 2, 3, 4, 5);
+
 	// Set Columns widths.
 	worksheet.column(1).setWidth(30);
-    worksheet.column(2).setWidth(20);
-	worksheet.column(3).setWidth(25);
+	worksheet.column(2).setWidth(30);
+    worksheet.column(3).setWidth(20);
 	worksheet.column(4).setWidth(25);
 	worksheet.column(5).setWidth(25);
-	worksheet.column(6).setWidth(35);
+	worksheet.column(6).setWidth(25);
+	worksheet.column(7).setWidth(35);
 	
 	// Loop over the stocks list
 	stocksList.forEach((stock,index) => {
-	// Skip inactive stocks
-	if(stock.isActive == false) return;
 	let rowNumber = index + 2;
 	let latestPrice = getLatestStockPrice(stock.data);
 	let dividendYield = 0;
@@ -139,21 +149,22 @@ function printAllStocksExcelFile(){
 	
 	if(stock.dividendAverage != null && stock.dividendAverage != 0){
 		dividendYield = latestPrice / stock.dividendAverage;
-		dividendYield = roundToTwo(dividendYield)
+		dividendYield = roundToX(dividendYield,2)
 	}
 	
 	if(stock.averageCost != null && stock.averageCost != 0) {
-		stockCostToItsAverageCost = (latestPrice-stock.averageCost)/stock.averageCost;
-		stockCostToItsAverageCost = roundToFour(stockCostToItsAverageCost)
 		stockAverageCost = stock.averageCost;
+		stockCostToItsAverageCost = (latestPrice-stock.averageCost)/stock.averageCost;
+		stockCostToItsAverageCost = roundToX(stockCostToItsAverageCost,4)
 	};
 	
-	worksheet.cell(rowNumber,1).string(stock.name).style(rowsStyle);
-	worksheet.cell(rowNumber,2).number(latestPrice).style(rowsStyle);
-	worksheet.cell(rowNumber,3).number(stock.dividendAverage).style(rowsStyle);
-	worksheet.cell(rowNumber,4).number(dividendYield).style(rowsStyle);
-	worksheet.cell(rowNumber,5).number(stockAverageCost).style(rowsStyle);
-	worksheet.cell(rowNumber,6).number(stockCostToItsAverageCost).style(rowsStyle);
+	worksheet.cell(rowNumber,1).string(stock.market).style(rowsStyle);
+	worksheet.cell(rowNumber,2).string(stock.name).style(rowsStyle);
+	worksheet.cell(rowNumber,3).number(latestPrice).style(rowsStyle);
+	worksheet.cell(rowNumber,4).number(stock.dividendAverage).style(rowsStyle);
+	worksheet.cell(rowNumber,5).number(dividendYield).style(rowsStyle);
+	worksheet.cell(rowNumber,6).number(stockAverageCost).style(rowsStyle);
+	worksheet.cell(rowNumber,7).number(stockCostToItsAverageCost).style(rowsStyle).style({numberFormat: '#.00%; -#.00%; -'});
   })
   workbook.write('output/stocksOutputExcel.xlsx');
 }
@@ -161,8 +172,6 @@ function printAllStocksExcelFile(){
 function setUpHttpRequests(stocksList) {
   let requestsList = [];
   stocksList.forEach(stock => {
-	  // Skip inactive stocks
-	  if(stock.isActive == false) return;
 	  let requiestUrl = url.replace('{STOCK_CODE}', stock.code);
 	  requestsList.push(axios.get(requiestUrl));
   })
@@ -171,7 +180,7 @@ function setUpHttpRequests(stocksList) {
 
 function getLatestStockPrice(stockData) {
   let stockLatestPrice = stockData[stockData.length - 1].indexPrice;
-  return roundToTwo(stockLatestPrice);
+  return roundToX(stockLatestPrice,2);
 }
 
 function convertNumberToSpecificPercision(num,percision){
@@ -181,10 +190,6 @@ function convertNumberToSpecificPercision(num,percision){
   return parseFloat(convertedNumber);
 }
 
-function roundToTwo(num) {    
-    return +(Math.round(num + "e+2")  + "e-2");
-}
-
-function roundToFour(num) {    
-    return +(Math.round(num + "e+4")  + "e-4");
+function roundToX(num,x) {
+    return +(Math.round(num + `e+${x}`)  + `e-${x}`);
 }
