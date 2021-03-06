1c9ccb72749ed5ea64cea96738444b4b1f28c4bf
Abdullah-Mubark <32361577+Abdullah-Mubark@users.noreply.github.com>
2021-03-07 00:33:36 +0300

    Delete search_stocks_in_google.ps1

--- search_stocks_in_google.ps1
+++
@@ -1,5 +0,0 @@
-foreach($stock_code in Get-Content .\stocks_codes.txt) {
-    if(-not ([string]::IsNullOrEmpty($stock_code))){
-        start chrome "https://www.google.com/search?q=tasi+${stock_code}"
-    }
-}
